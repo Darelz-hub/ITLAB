645c177c7079562e0bc91d5ffc273e483697b32b
Maksim <m.boltobogov@gmail.com>
2024-07-07 19:25:05 +0300

    Ещё одна проверка

--- SITEITLAB/settings.py
+++ SITEITLAB/settings.py
@@ -89,7 +89,7 @@ DATABASES = {
 'NAME': BASE_DIR / 'db.sqlite3',
 }
 }
-# ещё одна проверка
+# ещё одна проверка 12
 # DATABASES = {
 #     'default': {
 #         'ENGINE': 'django.db.backends.postgresql_psycopg2',
